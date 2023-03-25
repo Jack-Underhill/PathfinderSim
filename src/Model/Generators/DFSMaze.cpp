0724d43168a329bde05faaf7b166307a3cdbd8ab
Jack-Underhill <jack.underhill17@gmail.com>
2023-03-24 17:49:29 -0700

    Refactored & Commented DFS Generator

--- src/Model/Generators/DFSMaze.cpp
+++ src/Model/Generators/DFSMaze.cpp
@@ -1,71 +1,73 @@
-/* Jack Underhill & Jack Sanger
+/* Jack Underhill
  * CS 133, Spring 2022
  * Final Project - Pathfinding
  */
 #include "DFSMaze.h"
 
+/*
+ * Need to do for both DFS classes: backtrack continuously until done backtracking then return position key. 
+ * this way dont need to waste time animating backtracking that doesn't even visually update.
+ * --Actually it does animate backtracking for this DFS (gray to white) but not the other one.
+ */
+
 namespace PFSim {
-    
 namespace Generator {
 
     DFSMaze::DFSMaze(MazeGraph*& graph) : GeneratorTemplate(graph) 
     {
-        currNode = graph->getStartNode();
-        currBacktrackFrom = currNode->getDirectionMovedIn();
+        m_NodeStack.push( graph->getStartNode() );
     }
 
     int DFSMaze::step() 
     {
+        MazeNode* currNode = m_NodeStack.top();
         std::vector<DirectionMoved> availableMoves = getAvailableMoves(currNode);
         
         if(availableMoves.size() > 0) 
         {
-            m_stepCount++;
-            return stepNew(availableMoves);
+            stepNew(availableMoves, currNode);
+            AnimationObject::m_stepCount++;
         }
-        else 
+        else if(currNode->getType() != StartCell) 
         {
-            if(currNode->getType() == StartCell) 
-            {
-                setIsComplete(true);
-                currNode->setIsVisited(false);
-
-                return currNode->getPosition().positionKey;
-            }
-            else
-            {
-                return stepBacktrack();
-            }
+            stepBacktrack(currNode);
         }
+        else
+        {
+            AnimationObject::setIsComplete(true);
+            currNode->setIsVisited(false);
+        }
+
+        return currNode->getPosition().positionKey;
     }
 
     /*********************************************Private*********************************************/
 
-    std::vector<DirectionMoved> DFSMaze::getAvailableMoves(MazeNode*& curr) const 
+    std::vector<DirectionMoved> DFSMaze::getAvailableMoves(const MazeNode* curr) const 
     {
         std::vector<DirectionMoved> availableMoves;
-        NodePosition currPos = curr->getPosition();
+        int positionKey = curr->getPosition().positionKey;
         
-        NodePosition northPos(currPos.x, currPos.y - 1, m_MazeLength);
-        if(isAvailableMove(northPos)) 
+        int northKey = positionKey - GeneratorTemplate::m_MazeLength;
+        if(isAvailableMove(northKey)) 
         {
             availableMoves.push_back(NORTH);
         }
         
-        NodePosition westPos(currPos.x - 1, currPos.y, m_MazeLength);
-        if(isAvailableMove(westPos)) 
+        int westKey = positionKey - 1;
+        if(isAvailableMove(westKey) && GeneratorTemplate::isOnTheSameRow(westKey, curr)) 
         {
             availableMoves.push_back(WEST);
         }
         
-        NodePosition southPos(currPos.x, currPos.y + 1, m_MazeLength);
-        if(isAvailableMove(southPos)) 
+        int southKey = positionKey + GeneratorTemplate::m_MazeLength;
+        if(isAvailableMove(southKey)) 
         {
             availableMoves.push_back(SOUTH);
         }
         
-        NodePosition eastPos(currPos.x + 1, currPos.y, m_MazeLength);
-        if(isAvailableMove(eastPos)) 
+        int eastKey = positionKey + 1;
+        if(isAvailableMove(eastKey) && GeneratorTemplate::isOnTheSameRow(eastKey, curr)) 
         {
             availableMoves.push_back(EAST);
         }
@@ -73,11 +75,11 @@ namespace Generator {
         return availableMoves;
     }
 
-    bool DFSMaze::isAvailableMove(NodePosition checkPos) const 
+    bool DFSMaze::isAvailableMove(int positionKey) const
     {
-        if(isInsideMaze(checkPos, m_MazeLength)) 
+        if(GeneratorTemplate::isInsideMaze(positionKey)) 
         {
-            MazeNode* node = m_MappedNodes->at(checkPos.positionKey);
+            MazeNode* node = GeneratorTemplate::m_MappedNodes->at(positionKey);
             CellType type = node->getType();
 
             return (!node->isVisited() && type != BlankCell);
@@ -88,43 +90,24 @@ namespace Generator {
         }
     }
     
-    int DFSMaze::stepNew(std::vector<DirectionMoved> availableMoves)
+    void DFSMaze::stepNew(std::vector<DirectionMoved> availableMoves, MazeNode* currNode)
     {
-        if(currNode->isVisited()) 
-        {
-            currNode->setType(BlankCell);
-            currNode->setIsVisited(false);
-        }
-        else 
-        {
-            currNode->setIsVisited(true);
-            nodeStack.push(currNode);
-        }
-        MazeNode* temp = currNode;
+        currNode->setIsVisited(true);
 
-        DirectionMoved chosenMove = availableMoves[rand() % availableMoves.size()];
-        MazeNode* nodeMovedTo = connectNodes(currNode, chosenMove);
-        nodeMovedTo->setDirectionMovedIn(chosenMove);
+        DirectionMoved chosenDirection = availableMoves[rand() % availableMoves.size()];
+        MazeNode* nodeMovedTo = GeneratorTemplate::connectNodes(currNode, chosenDirection);
 
-        currNode = nodeMovedTo;
-
-        return temp->getPosition().positionKey;
+        nodeMovedTo->setDirectionMovedIn(chosenDirection);
+        m_NodeStack.push(nodeMovedTo);
     }
 
-    int DFSMaze::stepBacktrack()
+    void DFSMaze::stepBacktrack(MazeNode* currNode)
     {
-        currNode->setType(BlankCell);
         currNode->setIsVisited(false);
-
-        MazeNode* temp = currNode;
-
-        currNode = nodeStack.top();
-        nodeStack.pop();
-        currBacktrackFrom = currNode->getDirectionMovedIn();
-
-        return temp->getPosition().positionKey;
+        currNode->setType(BlankCell);
+        
+        m_NodeStack.pop();
     }
 
 } // namespace Generator
-
 } // namespace PFSim
--- src/Model/Generators/DFSMaze.h
+++ src/Model/Generators/DFSMaze.h
@@ -1,8 +1,20 @@
-/* Jack Underhill & Jack Sanger
- * CS 133, Spring 2022
- * Final Project - Pathfinding
- * The purpose of this class is to generate a randomized DFS maze for the pathfinder to navigate.
+/* 
+ * Jack Underhill
+ * Summer 2022
+ * Maze Generating and Pathfinding Simulator
+ * 
+ * Generator--Depth First Search Maze(DFSMaze)
+ * 
+ * This generating algorithm links every node in the given graph in a randomized depth first search.
+ * The links are undirected.
+ * 
+ * This implementation randomizes each step for a depth; instead of sticking to 1 direction for a
+ * depth in a normal DFS. For each step to be decided, a list of unvisited NWSE neighboring nodes
+ * is created and then 1 node from the list is chosen at random. This continues until there is an 
+ * empty list of unvisited neighboring nodes. Then backtrack the steps until the list is no longer
+ * empty and repeat from the top. The algorithm finishes once it has backtracked to the startnode.
  */
+
 #ifndef _DFSMaze_h_
 #define _DFSMaze_h_
 
@@ -15,45 +27,62 @@
 #include "MazeNode.h"
 
 namespace PFSim {
-    
 namespace Generator {
 
+    //Moves through the disconnected graph, connecting nodes in its path, until it reaches a
+    //deadend (no current-neighboring unvisited-cells). Then it backtrack its steps 
+    //until it can move into a neighboring unvisited cell (NextCell).
+    //This DFS is unique to normal DFS; it randomly chooses from its avaible moves to move into.
+    //Returns the cell it came from or nullptr if the graph is complete.
     class DFSMaze : public GeneratorTemplate 
     {
     public:
-        //Constructor takes a map of all the existing nodes to their location and a starting position
-        //to generate the graph from.
+        //Takes the given graph to store the starting node for this generating algorithm.
         DFSMaze(MazeGraph*& graph);
 
-        //Returns the animation's title to be displayed on the GUI while it runs.
+        //Returns the animation's title.
         std::string getTitle() const { return "Random DFS Maze"; }
 
+        //Returns the generator type.
         GeneratorType getGeneratorType() const { return GeneratorType::DFSMaze; }
-
-        //Moves through the disconnected graph, connecting nodes in its path, until it reaches a
-        //deadend (no current-neighboring unvisited-cells). Then it backtrack its steps 
-        //until it can move into a neighboring unvisited cell (NextCell).
-        //This DFS is unique to normal DFS; it randomly chooses from its avaible moves to move into.
-        //Returns the cell it came from or nullptr if the graph is complete.
+        
+        //Saves the current position to return at the end of the function.
+        //
+        //If there are an available unvisited node to move into, then call stepNew().
+        //Else if the current node is not the startcell (it hasnt backtracked back to the start), 
+        //   ---then call stepBacktrack().
+        //Else its the end of the algorithm and is marked for completion.
         int step();
 
     private:
-        std::stack<MazeNode*> nodeStack;
-        MazeNode* currNode; 
-        DirectionMoved currBacktrackFrom;
+        std::stack<MazeNode*> m_NodeStack;
 
-        // Check N,S,W,E and see if they are nullptr (available) and make a vector of available directions.
-        std::vector<DirectionMoved> getAvailableMoves(MazeNode*& curr) const;
+        //Check the N,S,W,E neighboring nodes, into isAvailableMove(), to return true. 
+        //Each neighbor that does return true: their direction from the curr node is added into a 
+        //vector to be returned.
+        std::vector<DirectionMoved> getAvailableMoves(const MazeNode* curr) const;
 
         //Returns true if a given position contains a cell which is valid to connect to.
-        bool isAvailableMove(NodePosition checkPos) const;
+        //
+        //Valid if:
+        //--The given position matches a node's position inside of the maze.
+        //--The matched node hasn't been visited/backtracked.
+        bool isAvailableMove(int positionKey) const;
 
-        int stepNew(std::vector<DirectionMoved> availableMoves);
-        int stepBacktrack();
+        //Open the given currNode by setting it to being visited.
+        //
+        //Out of the availableMoves, a direction to move in is randomly selected. The node moved 
+        //into is then linked with currNode.
+        //
+        //The node moved into is then added into the m_NodeStack.
+        void stepNew(std::vector<DirectionMoved> availableMoves, MazeNode* currNode);
+
+        //Display top node of the m_NodeStack as closed: set to unvisited and a blankcell.
+        //Pop the top node of the m_NodeStack.
+        void stepBacktrack(MazeNode* currNode);
     };
 
 } // namespace Generator
-
 } // namespace PFSim
 
 #endif
