c3f56b530ac0e8155f89d508dc9c151fad93d691
Jack-Underhill <jack.underhill17@gmail.com>
2023-03-24 17:50:48 -0700

    Refactored & Commented Prims Generator

--- src/Model/Generators/Prims.cpp
+++ src/Model/Generators/Prims.cpp
@@ -1,13 +1,11 @@
 #include "Prims.h"
 
 namespace PFSim {
-    
 namespace Generator {
 
     Prims::Prims(MazeGraph*& graph) : GeneratorTemplate(graph) 
     {
         int key = graph->getStartNode()->getPosition().positionKey;
-
         currNodeList.push_back(key);
     }
     
@@ -31,115 +29,139 @@ namespace Generator {
         }
     }
     
+    /*********************************************Private*********************************************/
+    
     int Prims::handleNextNode()
     {
-        int nodePositionKey = nextNodeList.top();
+        int currPositionKey = nextNodeList.top();
         nextNodeList.pop();
 
-        return nodePositionKey;
+        return currPositionKey;
     }
 
     int Prims::handleCurrNode()
     {
+        //randomly choose node from currNodeList.
         int randIndex = rand() % currNodeList.size();
-        int nodePositionKey = currNodeList[randIndex];
-        MazeNode* node = m_MappedNodes->at(nodePositionKey);
+        int currPositionKey = currNodeList[randIndex];
+        MazeNode* node = m_MappedNodes->at(currPositionKey);
 
-        updateNodes(node);
-        DirectionMoved dir = connectWithCompletedNode(node);
-        node->setDirectionMovedIn(dir);
-
+        //remove node from list.
         currNodeList[randIndex] = currNodeList.back();
         currNodeList.pop_back();
+        
+        //handle chosen node.
+        openNeighbors(node, currPositionKey);
+        std::vector<DirectionMoved> neighbors = getClosedNeighbors(node, currPositionKey);
+        updateValues(neighbors, node);
 
-        node->setIsVisited(false);
-        if(node->getType() != StartCell)
-        {
-            node->setType(BlankCell);
+        return currPositionKey;
+    }
+
+    void Prims::openNeighbors(MazeNode*& node, int currPositionKey)
+    { 
+        int northKey = currPositionKey - GeneratorTemplate::m_MazeLength;
+        if(isVerticallyValid(northKey)) 
+        { 
+            openNode(northKey); 
         }
 
-        return nodePositionKey;
+        int southKey = currPositionKey + GeneratorTemplate::m_MazeLength;
+        if(isVerticallyValid(southKey)) 
+        { 
+            openNode(southKey); 
+        }
+
+        int westKey = currPositionKey - 1;
+        if(isHorizontallyValid(westKey, node)) 
+        { 
+            openNode(westKey); 
+        }
+
+        int eastKey = currPositionKey + 1;
+        if(isHorizontallyValid(eastKey, node)) 
+        { 
+            openNode(eastKey); 
+        }
     }
 
-    void Prims::updateNodes(MazeNode*& node)
-    {
-        NodePosition checkNorth = copyPosition(node->getPosition());
-        checkNorth.y--;
-        checkIsNotHandled(checkNorth);
-        
-        NodePosition checkWest = copyPosition(node->getPosition());
-        checkWest.x--;
-        checkIsNotHandled(checkWest);
-        
-        NodePosition checkSouth = copyPosition(node->getPosition());
-        checkSouth.y++;
-        checkIsNotHandled(checkSouth);
-        
-        NodePosition checkEast = copyPosition(node->getPosition());
-        checkEast.x++;
-        checkIsNotHandled(checkEast);
-    }
-
-    DirectionMoved Prims::connectWithCompletedNode(MazeNode*& node) const
+    std::vector<DirectionMoved> Prims::getClosedNeighbors(MazeNode*& node, int currPositionKey) const
     {
         std::vector<DirectionMoved> neighbors;
 
-        //add valid nodes, to connect with, into "neighbors"
-        NodePosition checkNorth = copyPosition(node->getPosition());
-        checkNorth.y--;
-        checkIsCompletedNode(checkNorth, NORTH, neighbors);
+        int northKey = currPositionKey - GeneratorTemplate::m_MazeLength;
+        if(isVerticallyValid(northKey) && isClosed(northKey)) 
+        { 
+            neighbors.push_back(NORTH); 
+        }
 
-        NodePosition checkWest = copyPosition(node->getPosition());
-        checkWest.x--;
-        checkIsCompletedNode(checkWest, WEST, neighbors);
+        int southKey = currPositionKey + GeneratorTemplate::m_MazeLength;
+        if(isVerticallyValid(southKey) && isClosed(southKey)) 
+        {  
+            neighbors.push_back(SOUTH); 
+        }
 
-        NodePosition checkSouth = copyPosition(node->getPosition());
-        checkSouth.y++;
-        checkIsCompletedNode(checkSouth, SOUTH, neighbors);
+        int westKey = currPositionKey - 1;
+        if(isHorizontallyValid(westKey, node) && isClosed(westKey)) 
+        { 
+            neighbors.push_back(WEST); 
+        }
 
-        NodePosition checkEast = copyPosition(node->getPosition());
-        checkEast.x++;
-        checkIsCompletedNode(checkEast, EAST, neighbors);
+        int eastKey = currPositionKey + 1;
+        if(isHorizontallyValid(eastKey, node) && isClosed(eastKey)) 
+        { 
+            neighbors.push_back(EAST); 
+        }
 
-        //Choose a random valid node from the list to connect together and return the direction moved in. 
-        //If there are no valid nodes, then return CENTER.
+        return neighbors;
+    }    
+
+    void Prims::openNode(int positionKey)
+    {
+        MazeNode* node = m_MappedNodes->at(positionKey);
+
+        //Note: checking and setting isVisited prevents handling the same node multiple times.
+        if(node->getType() == GenerationCell && 
+           !node->isVisited())
+        {
+            nextNodeList.push(positionKey);
+            currNodeList.push_back(positionKey);
+            node->setIsVisited(true);
+        }
+    }
+
+    bool Prims::isVerticallyValid(int key) const
+    {
+        return GeneratorTemplate::isInsideMaze(key);
+    }
+
+    bool Prims::isHorizontallyValid(int newKey, MazeNode*& prevNode) const
+    {
+        return (GeneratorTemplate::isInsideMaze(newKey) && GeneratorTemplate::isOnTheSameRow(newKey, prevNode));
+    }
+    
+    bool Prims::isClosed(int key) const
+    {
+        return (m_MappedNodes->at(key)->getType() != GenerationCell);
+    }
+    
+    void Prims::updateValues(std::vector<DirectionMoved>& neighbors, MazeNode*& node) const
+    {
+        //Condition will only be false (size = 0) for the startnode (because no nodes have been closed yet).
         if(neighbors.size() > 0)
         {
-            DirectionMoved dir = neighbors[ rand() % neighbors.size() ];
-            connectNodes(node, dir);
+            DirectionMoved chosenDir = neighbors[ rand() % neighbors.size() ];
+            GeneratorTemplate::connectNodes(node, chosenDir);
 
-            return inverseDirection(dir);
-        }
-        else
-        {
-            return CENTER;
-        }
-    }
+            chosenDir = GeneratorTemplate::inverseDirection(chosenDir);
+            node->setDirectionMovedIn(chosenDir);
 
-    void Prims::checkIsNotHandled(NodePosition& pos)
-    {
-        pos.updatePositionKey(m_MazeLength);
-
-        /*Note: checking and setting isVisited prevents handling the same node multiple times.*/
-        if(isInsideMaze(pos, m_MazeLength) && 
-           m_MappedNodes->at(pos.positionKey)->getType() == GenerationCell && 
-           !m_MappedNodes->at(pos.positionKey)->isVisited())
-        {
-            nextNodeList.push(pos.positionKey);
-            currNodeList.push_back(pos.positionKey);
-            m_MappedNodes->at(pos.positionKey)->setIsVisited(true);
+            node->setType(BlankCell);
         }
-    }
 
-    void Prims::checkIsCompletedNode(NodePosition& pos, const DirectionMoved& dir, std::vector<DirectionMoved>& neighbors) const
-    {
-        pos.updatePositionKey(m_MazeLength);
-        if(isInsideMaze(pos, m_MazeLength) && m_MappedNodes->at(pos.positionKey)->getType() != GenerationCell)
-        {
-            neighbors.push_back(dir);
-        }
+        //Close node
+        node->setIsVisited(false);
     }
 
 } // namespace Generator
-
 } // namespace PFSim
--- src/Model/Generators/Prims.h
+++ src/Model/Generators/Prims.h
@@ -1,3 +1,21 @@
+/* 
+ * Jack Underhill
+ * Summer 2022
+ * Maze Generating and Pathfinding Simulator
+ * 
+ * Generator--Prims
+ * 
+ * This generating algorithm links every node in the given graph into a minimum spanning tree.
+ * The links are undirected.
+ * 
+ * This implementation doesn't have a weighted graph, so instead of expanding to the minimum
+ * weighted node, randomness is used for expanding the tree. A random unopened-node from the
+ * fringe(nodes that are adjacent to the perimeter of the tree outwardly) is chosen to link to its
+ * adjacent closed node. If there are multiple neighboring/adjacent closed nodes then one of them
+ * is chosen at random. This process continues until every node in the graph has been linked and 
+ * closed.
+ */
+
 #ifndef _Prims_h_
 #define _Prims_h_
 
@@ -8,36 +26,71 @@
 #include "MazeNode.h"
 
 namespace PFSim {
-    
 namespace Generator {
     
     class Prims : public GeneratorTemplate
     {
     public:
+        //Takes the given graph to store the starting node into currNodeList for this generating 
+        //algorithm.
         Prims(MazeGraph*& graph);
 
+        //Returns the animation's title.
         std::string getTitle() const { return "Prim's Maze"; }
 
+        //Returns the generator type.
         GeneratorType getGeneratorType() const { return GeneratorType::Prims; }
 
+        //Handles and returns a node from nextNodeList if nextNodeList isn't empty, else handles 
+        //and returns a node from currNodeList. If both lists are empty then Prims is marked 
+        //complete.
         int step();
 
     private:
         std::vector<int> currNodeList;
         std::stack<int> nextNodeList;
 
+        //Returns the top node's position key from the nextNodeList and removes it from the list.
         int handleNextNode();
+
+        //Returns a randomly chosen node from the currNodeList after handling it and removing it 
+        //from the list.
         int handleCurrNode();
 
-        void updateNodes(MazeNode*& node);
-        DirectionMoved connectWithCompletedNode(MazeNode*& node) const;
+        //The given node's NWSE neighboring nodes are opened if they are valid and in bounds.
+        void openNeighbors(MazeNode*& node, int nodePositionKey);
+
+        //The node of the given key is opened if it has not been processed already. 
+        //Opening a node consists of: setting it to visited, and adding it to both currNodeList 
+        //and nextNodeList.
+        void openNode(int positionKey);
+
+        //Returns a list of the NWSE neighboring nodes, of the given node/positionKey, that're
+        //closed, valid and in bounds.
+        std::vector<DirectionMoved> getClosedNeighbors(MazeNode*& node, int nodePositionKey) const;
+
+        //Only if the given neighbors list isn't empty:
+        //The given node is linked to a random node from the given neighbors list. The given node's
+        //DirectionMovedIn is set towards its freshly linked neighbor, and then the given node is
+        //set to a BlankCell.
+        //
+        //The given node is then always set to closed.
+        void updateValues(std::vector<DirectionMoved>& neighbors, MazeNode*& node) const;
+
+        //Returns true and is valid if the given node position key is inside the bounds of the 
+        //graph.
+        bool isVerticallyValid(int key) const;
+
+        //Returns true and is valid if the given node position key is inside the bounds of the 
+        //graph and is on the same row of the given prevNode.
+        bool isHorizontallyValid(int newKey, MazeNode*& prevNode) const;
         
-        void checkIsNotHandled(NodePosition& pos);
-        void checkIsCompletedNode(NodePosition& pos, const DirectionMoved& dir, std::vector<DirectionMoved>& neighbors) const;
+        //The node of the given key is considered closed if it's cell type is no longer a 
+        //GenerationCell; if so then this function returns true.
+        bool isClosed(int key) const;
     };
 
 } // namespace Generator
-
 } // namespace PFSim
 
 #endif
