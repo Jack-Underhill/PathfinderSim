270a1de35ef6ae165110035bf9a8447f43f5cd7e
Jack-Underhill <jack.underhill17@gmail.com>
2023-03-24 17:35:35 -0700

    Refactored & Commented AStar

--- src/Model/Pathfinders/AStar.cpp
+++ src/Model/Pathfinders/AStar.cpp
@@ -1,59 +1,102 @@
-/* Jack Underhill
- * CS 133, Spring 2022
- * Final Project - Pathfinding
+/* 
+ * Jack Underhill
+ * Summer 2022
+ * Maze Generating and Pathfinding Simulator
  */
 #include "AStar.h"
 
 namespace PFSim {
 namespace Pathfinder {
 
-
     /*
-        BUG: the pathing solutions for: instant repathing and clicking run AStar are (sometimes) different..
+        BUG: the pathing solutions for: instant repathing and clicking run AStar are (sometimes) different.
+        -update, instantly crashes now.
+        -update(12/11), runs but skips all CP's.
+        -update(12/12), works except its not pathing the same as the normal sim.
+            -Example, instead of zigzagging its way to a cp diagonally away, it will move all x, then all y to cp.
+            -Im guessing its a instant-repathing logic issue that isnt using the AStar algorithm the same way as the normal sim.
+
+        Readablity for: function variable names. and function names need review.
+
+        Reorganise functions between public, protected, private.
     */
-   
     
-    bool MinHeapAStar::push(HeapProps* props)
+    
+    bool AStarHeap::push(HeapProp* prop)
     {
-        int key = ((HeapPropsAStar*)props)->node->getPosition().positionKey;
+        int key = ((AStarHeapProp*)prop)->node->getPosition().positionKey;
         int index = find(key);
 
-        if(index == -1)
+        if(index == -1) //new key
         {
-            m_PropVector.push_back(props);
-            bubbleUp();
+            m_PropVector.push_back(prop);
+            MinHeap::bubbleUp();
+
+            return true;
         }
-        else
+        else //duplicate key
         {
-            HeapProps* curr = m_PropVector[index];
-
-            if(((HeapPropsAStar*)curr)->distanceG > ((HeapPropsAStar*)props)->distanceG)
-            {
-                // update optimized distanceG
-                ((HeapPropsAStar*)curr)->distanceG = ((HeapPropsAStar*)props)->distanceG;
-
-                bubbleUp(index);
-            }
-            else
-            {
-                return false;
-            }
+            return handleDuplicate(prop, index);
         }
-
-        return true;
     }
 
-    int MinHeapAStar::find(int key) const 
+    int AStarHeap::find(int key) const // try to optimize or remove the need for this function
     {
         for(int i = 0; i < m_PropVector.size(); i++)
         {
-            if(((HeapPropsAStar*)m_PropVector[i])->node->getPosition().positionKey == key)
+            AStarHeapProp* prop = (AStarHeapProp*)m_PropVector[i];
+            int propKey = prop->node->getPosition().positionKey;
+            
+            if(propKey == key)
             {
                 return i;
             }
         }
 
         return -1;
+    }
+    
+    bool AStarHeap::handleDuplicate(HeapProp* newProp, int index)
+    {
+        HeapProp* oldProp = m_PropVector[index];
+
+        bool isBetter = ((AStarHeapProp*)oldProp)->distanceG > ((AStarHeapProp*)newProp)->distanceG;
+        if(isBetter)
+        {
+            // update optimized distanceG
+            ((AStarHeapProp*)oldProp)->distanceG = ((AStarHeapProp*)newProp)->distanceG;
+
+            MinHeap::bubbleUp(index);
+        }
+
+        delete newProp;
+        return isBetter;
+    }
+
+    std::vector<HeapProp*> AStarHeap::getTiedHeapProps()
+    {
+        std::vector<HeapProp*> list;
+        getTiedHeapPropsHelper(list, 0, m_PropVector[0]->getValue());
+
+        return list;
+    }
+
+    void AStarHeap::getTiedHeapPropsHelper(std::vector<HeapProp*>& list, int currIndex, int value)
+    {
+        if(value == m_PropVector[currIndex]->getValue())
+        {   
+            list.push_back( m_PropVector[currIndex] );
+
+            if(MinHeap::hasLeftChild(currIndex))
+            {
+                getTiedHeapPropsHelper(list, MinHeap::getLeftChildIndex(currIndex), value);
+            }
+
+            if(MinHeap::hasRightChild(currIndex))
+            {
+                getTiedHeapPropsHelper(list, MinHeap::getRightChildIndex(currIndex), value);
+            }
+        }
     }
     
     /*------------------------------------------AStar-----------------------------------------*/
@@ -63,57 +106,52 @@ namespace Pathfinder {
         m_MazeLength = graph->getMazeLength();
         m_EndPosition = graph->getEndNode()->getPosition();
 
-        m_MinHeap = new MinHeapAStar();
+        m_MinHeap = new AStarHeap();
 
         m_PrevDistanceG = (-1 * NON_DIAGONAL_COST);
         m_IsDiagonalMove = false;
 
         // set starting node for AStar's algorithm.
-        if(graph->getLastFoundTarget() == nullptr)
-        {
-            openNode( graph->getStartNode() );
-        }
-        else
-        {
-            openNode( graph->getLastFoundTarget() );
-        }
+        MazeNode* currNode = PathfinderTemplate::getStartingPlace();
+        HeapProp* props = new AStarHeapProp(currNode, 0, 0);
+        m_MinHeap->push(props);
     }
     
     AStar::~AStar()
     {
         delete m_MinHeap;
     }
-
+    
     int AStar::currStep()
     {
-        MazeNode* curr = ((HeapPropsAStar*)m_MinHeap->top())->node;
-        m_PrevDistanceG = ((HeapPropsAStar*)m_MinHeap->top())->distanceG;
-        closeNode();
+        MazeNode* curr = getCurrentNodeStep();
 
-        if(m_TargetListSize == 0 && curr->getType() == EndCell)
-        {
-            setIsComplete(true);
-            m_TargetNodeFound = curr;
-            m_IsStillSearching = false;
-        }
-        else if((curr->getType() == CheckpointCell && !removeTargetIfContained(curr)) || 
-                 curr->getType() != CheckpointCell)
-        {
-            curr->setIsVisited(true);
-            curr->setIsNext(false);
-
-            m_IsDiagonalMove = false;
-            addAvailableMoves(curr); //non-diagonal moves
-            m_IsDiagonalMove = true;
-            addDiagonalAvailableMoves(curr);
-
-            if(m_MinHeap->isEmpty())
-            {
-                m_IsStillSearching = false;
-            }
-        }
+        PathfinderTemplate::currStepTemplate(curr);
 
         return curr->getPosition().positionKey;
+    }
+    
+    MazeNode* AStar::getCurrentNodeStep()
+    {
+        handleTies();
+        MazeNode* curr =  ((AStarHeapProp*)m_MinHeap->top())->node;
+        m_PrevDistanceG = ((AStarHeapProp*)m_MinHeap->top())->distanceG;
+        closeNode(curr);
+
+        return curr;
+    }
+    
+    void AStar::updatePathfinderStep(MazeNode*& currNode)
+    {
+        m_IsDiagonalMove = false;
+        addAvailableMoves(currNode);//non-diagonal moves
+        m_IsDiagonalMove = true;
+        addDiagonalAvailableMoves(currNode);//diagonal moves
+
+        if(m_MinHeap->isEmpty())
+        {
+            m_IsStillSearching = false;
+        }
     }
 
     int AStar::heuristicEquation(int x1, int y1, int x2, int y2) const
@@ -148,12 +186,12 @@ namespace Pathfinder {
     {
         if(m_TargetListSize > 0)
         {
-            double closestTargetDistance = heuristicEquationWithKey(pos, m_TargetList[0]);
+            int closestTargetDistance = heuristicEquationWithKey(pos, m_TargetList[0]);
 
             for(int i = 1; i < m_TargetListSize; i++)
             {
                 int currTargetDistance = heuristicEquationWithKey(pos, m_TargetList[i]);
-
+                
                 if(closestTargetDistance > currTargetDistance)
                 {
                     closestTargetDistance = currTargetDistance;
@@ -171,6 +209,7 @@ namespace Pathfinder {
     bool AStar::openNode(MazeNode* curr)
     {
         double stepDistance;
+
         if(m_IsDiagonalMove)
         {
             //doing -1 gives diagional moves higher priority than non-diagional that would've had the same Fscore.
@@ -181,35 +220,31 @@ namespace Pathfinder {
             stepDistance = NON_DIAGONAL_COST;
         }
 
-        HeapProps* props = new HeapPropsAStar(curr, m_PrevDistanceG + stepDistance, getHeuristicDistance( curr->getPosition() ));
-        // std::cout << "push: " << curr->getPosition().positionKey << " with F = " << props->getFScore() << std::endl;//
+        HeapProp* props = new AStarHeapProp(curr, m_PrevDistanceG + stepDistance, getHeuristicDistance( curr->getPosition() ));
 
         return m_MinHeap->push(props);
     }
 
-    void AStar::closeNode()
-    {
-        // std::cout << "CLOSED: " << m_MinHeap->top()->node->getPosition().positionKey << " with F = " << m_MinHeap->top()->getFScore() << std::endl;//
-        MazeNode* curr = ((HeapPropsAStar*)m_MinHeap->top())->node;
-        
+    void AStar::closeNode(MazeNode* node)
+    {    
         m_MinHeap->pop();
 
-        curr->setIsNext(false);
-        curr->setIsVisited(true);
+        node->setIsNext(false);
+        node->setIsVisited(true);
     }
 
     void AStar::addIfAvailable(MazeNode*& curr, MazeNode*& prev, DirectionMoved dir)
     {
         if(PathfinderTemplate::isAvailableMove(curr)) 
         {
-            bool hasParentChanged = openNode(curr);
+            bool didOpenNode = openNode(curr);
 
             if(!curr->isNext())
             {
-                setNext(curr);
+                PathfinderTemplate::setNext(curr);
             }
 
-            if(hasParentChanged)
+            if(didOpenNode)
             {
                 curr->setDirectionMovedIn(dir);
                 curr->parent = prev;
@@ -228,16 +263,15 @@ namespace Pathfinder {
     bool AStar::isValidZigZagConnection(MazeNode*& curr) const
     {
         bool isNonWall = (curr != nullptr && curr->getType() != WallCell);
-        bool hasBeenQueued = (curr != nullptr && (curr->isNext() || curr->isVisited()));
+        bool isOpen = (curr != nullptr && curr->isNext());
 
-        return (isNonWall && hasBeenQueued); 
+        return (isNonWall && isOpen); 
     }
 
     void AStar::handleDiagonalMove(MazeNode*& curr, DiagonalDirection dir)
     {
         if(dir == NE)
         {
-            // NORTH-EAST Diagonal
             if(isValidZigZagConnection(curr->N))
             {
                 addIfAvailable(curr->N->E, curr->N, EAST);
@@ -249,7 +283,6 @@ namespace Pathfinder {
         }
         else if(dir == NW)
         {
-            //NORTH-WEST Diagonal
             if(isValidZigZagConnection(curr->N))
             {
                 addIfAvailable(curr->N->W, curr->N, WEST);
@@ -261,7 +294,6 @@ namespace Pathfinder {
         }
         else if(dir == SE)
         {
-            //SOUTH-EAST Diagonal
             if(isValidZigZagConnection(curr->S))
             {
                 addIfAvailable(curr->S->E, curr->S, EAST);
@@ -273,7 +305,6 @@ namespace Pathfinder {
         }
         else if(dir == SW)
         {
-            //SOUTH-WEST Diagonal
             if(isValidZigZagConnection(curr->S))
             {
                 addIfAvailable(curr->S->W, curr->S, WEST);
@@ -283,6 +314,72 @@ namespace Pathfinder {
                 addIfAvailable(curr->W->S, curr->W, SOUTH);
             }
         }
+    }
+
+    void AStar::handleTies()
+    {
+        if(m_TargetListSize > 1)
+        {
+            std::vector<HeapProp*> list = m_MinHeap->getTiedHeapProps();
+
+            //Need multiple nodes in tie for a tie breaker.
+            if(list.size() > 1)
+            {
+                int index = getIndexOfLowestH(list);
+                updateHeapWithTieBreaker(list, index);
+            }
+        }
+    }
+    
+    void AStar::updateHeapWithTieBreaker(std::vector<HeapProp*>& listOfTiedNodes, int indexOfTieBreaker)
+    {
+        //remove all tied values
+        for(int i = 0; i < listOfTiedNodes.size(); i++) 
+        {
+            copyHeapProp(listOfTiedNodes, i);
+            m_MinHeap->pop();
+        }
+
+        //swap priority move to first in list
+        HeapProp* temp = listOfTiedNodes[0];
+        listOfTiedNodes[0] = listOfTiedNodes[indexOfTieBreaker];
+        listOfTiedNodes[indexOfTieBreaker] = temp;
+
+        //re-enter rest of tied values
+        for(int i = 0; i < listOfTiedNodes.size(); i++) 
+        {
+            m_MinHeap->push(listOfTiedNodes[i]);
+        }
+    }
+    
+    int AStar::getIndexOfLowestH(std::vector<HeapProp*>& list)
+    {
+        int key;
+        int smallestHValue = pow(m_MazeLength, 2) * NON_DIAGONAL_COST;
+
+        for(int i = 0; i < list.size(); i++)
+        {
+            int currH = ((AStarHeapProp*)list[i])->distanceH;
+
+            if(currH < smallestHValue) 
+            {
+                smallestHValue = currH;
+                key = i;
+            }
+        }
+
+        return key;
+    }
+    
+    void AStar::copyHeapProp(std::vector<HeapProp*>& list, int index)
+    {
+        MazeNode* node  = ((AStarHeapProp*)list[index])->node;
+        int HVal        = ((AStarHeapProp*)list[index])->distanceH;
+        int GVal        = ((AStarHeapProp*)list[index])->distanceG;
+
+        HeapProp* newProp = new AStarHeapProp(node, GVal, HVal);
+
+        list[index] = newProp;
     }
     
 } // namespace Pathfinder
--- src/Model/Pathfinders/AStar.h
+++ src/Model/Pathfinders/AStar.h
@@ -1,59 +1,31 @@
-/*
-    Astar will use an internal map of each nodes key to a stuct containing:
-
-    Distance traveled(steps) from the start (G).
-    Heuristic distance(displacement from current position to the end position) (H).
-    Total value => (f = g + h).
-    Previous node.
-
-    Somehow have to weighting a priority queue with the lowest F value. (min-heap tree?)
-
-    So a PQ stores all data into a min-heap tree and then it pulls the (current)min value from the heap 1 at a time into a queue. 
-    (In other words its a 2 step data type (heap then queue))
-
-    chose node.
-    check/open every available move and calculate their f value, add it into the heap
-    close/pop the current node.
-    choose the smallest f value from the heap to be the next chosen node.
-    add the rest of the queues values into a heap
-    then restart from the first step with the chosen node
-    ends once the end node is found
-
-    Note: if a available move is a node that has already been addressed from another node, recalculate its f value from the current node and if its better then replace its values.
-
-    Note: The ordering of the steps might need to be shuffled for optimization later on.
-
-    Note: Open nodes are next nodes. and closed nodes are curr nodes
-*/
-
-/*
-
-A* pseudocode
-
-astar(v1, v2):
-    consider every vertex to have a cost of infinity, except v1 which has a cost of 0.
-    create a priority queue of vertexes, ordered by (cost+heuristic), storing only v1
-    with a priority of H(v1, v2).
-
-while the pqueue is not empty:
-    dequeue a vertex v from the pqueue, and mark it as visited.
-    for each of the unvisited neighbors n of v, we now know that we can reach
-    this neighbor with a total cost of (v's cost + the weight of the edge from v to n).
-        if the neighbor is not in the pqueue, or this is cheaper than n's current cost,
-        we should enqueue the neighbor n to the pqueue with this new cost
-        plus H(n, v2), and with v as its previous vertex.
-
-when we are done, we can reconstruct the path from v2 back to v1
-by following the previous pointers.
-
-* (basically, add H(...) to costs of elements in PQ to improve PQ processing order)
-
-*/
-
-/* Jack Underhill
- * CS 133, Spring 2022
- * Final Project - Pathfinding
+/* 
+ * Jack Underhill
+ * Summer 2022
+ * Maze Generating and Pathfinding Simulator
+ * 
+ * Pathfinder--A* search algorithm(AStar)
+ * 
+ * This pathfinder algorithm makes the lowest-cost path from the start node to the closest target 
+ * node. This means the algorithm prioritizes choosing moves that advance itself in getting closer 
+ * to the target; before choosing alternate moves.
+ * 
+ * The lowest-cost move is calculated by this function: f(n) = g(n) + h(n).
+ *      f(n) = F value      (Total path cost)
+ *      g(n) = G distance   (Step count from start)
+ *      h(n) = H distance   (Heuristic (Manhatten) distance to the target)
+ * 
+ * Before every (next lowest-cost) move, every neighbor node that hasn't been calculated/open yet 
+ * needs to be calculated and added into the open list. (Neighbors are considered one step away)
+ * Then from the open list, the next lowest-cost move is selected and removed from the open 
+ * list to be marked completed/closed. Repeat.
+ * 
+ * This implementation of AStar uses a min heap to store the open node's F values. This way the 
+ * min F value (top of the heap) is always considered the next lowest-cost move. 
+ * This is achieved by using a overided heap (AStarHeap) to use AStar props (AStarHeapProp). Each 
+ * node opened creates an AStar props to be pushed into the AStar Heap. The props stored are: the 
+ * node itself, its G distance and its H distance.
  */
+
 #ifndef _AStar_h_
 #define _AStar_h_
 
@@ -70,6 +42,9 @@ by following the previous pointers.
 namespace PFSim {
 namespace Pathfinder {
 
+    /*
+     * Enum representations for all 4 diagonal directions.
+     */
     enum DiagonalDirection
     {
         NW,
@@ -78,54 +53,105 @@ namespace Pathfinder {
         SE
     };
 
-
-    class MinHeapAStar : public MinHeap
-    {
-    public:
-        MinHeapAStar() = default;
-        virtual ~MinHeapAStar() = default;
-
-        bool push(HeapProps* props);
-
-    private:
-        int find(int key) const;
-    };
-
-
-    class HeapPropsAStar : public HeapProps
+    /*
+     * Properties that are stored; for each node opened from AStar's algorithm.
+     */
+    class AStarHeapProp : public HeapProp
     {
     public:
         MazeNode* node;     // Node's posisiton Key
         int distanceG;      // Step count from start
         int distanceH;      // Heuristic (Manhatten) distance
 
-        int getValue() const { return distanceG + distanceH; } // F value
-
-        HeapPropsAStar(MazeNode* node, int distanceG, int distanceH)
+        //Takes the node itself and its G & H distances.
+        AStarHeapProp(MazeNode* node, int distanceG, int distanceH)
         {
             this->node = node;
             this->distanceG = distanceG;
             this->distanceH = distanceH;
         }
-        ~HeapPropsAStar() = default;
+
+        //Defualt destructor.
+        ~AStarHeapProp() = default;
+
+        //Returns F value of the current node (G distance + H distance).
+        int getValue() const { return distanceG + distanceH; }
+    };
+
+    /*
+     * Overided MinHeap to be able to store AStarHeapProp and evaluate elements by 
+     * each AStarHeapProp's F value.
+     */
+    class AStarHeap : public MinHeap
+    {
+    public:
+        //Defualt constructor.
+        AStarHeap() = default;
+        
+        //Defualt destructor.
+        ~AStarHeap() = default;
+        
+        //Returns true if the given prop has been pushed into the heap.
+        //
+        //Returns false if not pushed into the heap; 
+        //the given prop contained a node that is already in one of the heap-stored props, 
+        //and the given copy doesn't optimize the node's G-distance.
+        bool push(HeapProp* prop);
+
+        //Returns a vector list of all the HeapProps that are tied for the minimum value.
+        std::vector<HeapProp*> getTiedHeapProps();
+
+    private:
+        //Searches through the whole m_PropVector to see if the current given node key already 
+        //exists in the list.
+        //Returns the index of the found duplicate, else returns -1.
+        int find(int key) const;
+
+        //Returns true if the given prop's G-distance is less than the heap-stored prop's 
+        //(at the given index) G-distance. The G-distance of the heap-stored prop is replaced with 
+        //the given one and then bubbled up into place inside the heap.
+        //
+        //Else returns false and no heap updating.
+        bool handleDuplicate(HeapProp* prop, int index);
+
+        //Adds HeapProp at currIndex to the referenced list if its value is equal to the given value.
+        //If so then the left and right children are recursively called in place of the currIndex.
+        void getTiedHeapPropsHelper(std::vector<HeapProp*>& list, int currIndex, int value); 
     };
     
-    
+    /*
+     * Astar Pathfinding Algorithm.
+     */
     class AStar : public PathfinderTemplate 
     {
     public:
-        //Constructor takes the start, end, and a list of checkpoints the pathfinder must hit.
+        //Takes the given graph to store: 
+        //--the starting node for this pathfinding algorithm.
+        //--the end node's position.
+        //--the maze's length.
+        //
+        //The rest of the member variables are then initialized by their own. 
         AStar(MazeGraph*& graph);
+
         ~AStar();
 
-        //Returns the animation's title to be displayed on the GUI while it runs.
+        //Returns the animation's title.
         std::string getTitle() const { return "AStar"; }
 
+        //Returns the pathfinder type.
         PathfinderType getPathfinderType() const { return PathfinderType::AStar; }
 
+    protected:
+        //Handles/updates tied top nodes (if there is).
+        //Takes and closes the top node from the m_MinHeap to be returned.
+        MazeNode* getCurrentNodeStep();
+        
+        //addIfAvailable() for each (N,W,S,E,NE,NW,SE,SW) neighboring nodes into the m_MinHeap.
+        //If the m_MinHeap is still empty after, then set the algorithm to be no longer searching.
+        void updatePathfinderStep(MazeNode*& currNode);
+
     private:
-        MinHeapAStar* m_MinHeap;
-        std::unordered_set<int> m_Set;
+        AStarHeap* m_MinHeap;
 
         NodePosition m_EndPosition;
         int m_MazeLength;
@@ -134,20 +160,70 @@ namespace Pathfinder {
 
         const int NON_DIAGONAL_COST = 10;
         const int DIAGONAL_COST = 14;
-        
+
+        //getCurrentNodeStep() to pass into PathfinderTemplate::currStepTemplate(),
+        //and then return the currNode's position key.
         int currStep();
+
+        //If it is available then: 
+        //set the curr node to next (if not already),
+        //set the given moved direction to the given curr node (if successfully opened curr node),
+        //set the curr node's parent node to be the given prev node (if successfully opened curr node).
         void addIfAvailable(MazeNode*& curr, MazeNode*& prev, DirectionMoved dir);
 
+        //Returns the H-distance cost from position 1 to position 2. 
+        //The heuristic equation is simular to manhattan distance but prioritizes zigzaging rather 
+        //than moving full x-direction then full y-direction.
+        //
+        //Each diagonal move cost (1 x-move and 1 y-move) = 14.
+        //Each non-diagonal move cost (1 x-move or 1 y-move) = 10.
         int heuristicEquation(int x1, int y1, int x2, int y2) const;
-        int heuristicEquationWithKey(const NodePosition& pos, int positionKey) const;
+
+        //Converts the pos and positionKey to (x,y) coordinates to pass into heuristicEquation().
+        //Returns the H-distance cost from pos to positionKey.
+        int heuristicEquationWithKey(const NodePosition& pos, int positionKey) const;                    // rename variable names
+
+        //If there are checkpoints remaining: H-distance to each checkpoint is calculated and the 
+        //smallest one is returned.
+        //Else the H-distance to the end node is calculated and returned.
         int getHeuristicDistance(const NodePosition& pos) const;
 
+        //Open/push the given curr node into the heap. The G-distance cost and the H-distance cost 
+        //is calculated into the node's properties to be pushed all together.
+        //Returns true if the node was opened, else false.
         bool openNode(MazeNode* curr);
-        void closeNode();
 
+        //Close/pop the top node in the heap, and set it to be no longer next and now visited.
+        void closeNode(MazeNode* node);
+
+        //Call handleDiagonalMove() for all 4 diagonal moves (NE, NW, SE, SW).
         void addDiagonalAvailableMoves(MazeNode*& curr);
+
+        //If the given diagonal directed move returns true from isValidZigZagConnection(), 
+        //then pass the move into addIfAvailable().
         void handleDiagonalMove(MazeNode*& curr, DiagonalDirection dir);
+
+        //Returns true if the given curr node is not a wallcell and is open, else returns false.
         bool isValidZigZagConnection(MazeNode*& curr) const;
+
+        //Tied nodes for the top of the m_MinHeap need to be broken when deciding for which target
+        //to move towards; or else steps would be wasted moving towards every target equally away.
+        //So a tiebreaker is only called when there are multiple active targets and there are
+        //multiple nodes tied for the top of the m_MinHeap. 
+        void handleTies();
+        
+        //With the given listOfTiedNodes, every node from there is copied and removed from the
+        //heap, and then added back into the heap with the given indexOfTieBreaker (the node chosen
+        //from the tie) to always be at the top of the heap.
+        void updateHeapWithTieBreaker(std::vector<HeapProp*>& listOfTiedNodes, int indexOfTieBreaker);
+
+        //Goes through the given list of tied HeapProps and returns the index of the node with the
+        //smallest H-distance.
+        int getIndexOfLowestH(std::vector<HeapProp*>& list);
+
+        //Replace the given index of the given list with a new allocated HeapProp that is a copy of
+        //the old HeapProp (the one being replaced).
+        void copyHeapProp(std::vector<HeapProp*>& list, int index);
     };
     
 } // namespace Pathfinder
